6e0e449a0fc46bddb6142375fb65174cf5cb0875
Lucas Ciceri <lucasciceri59@gmail.com>
2019-07-24 21:36:33 -0300

    alta user api

--- src/controladores/api.controlador.js
+++ src/controladores/api.controlador.js
@@ -15,14 +15,66 @@ exports.login = async function (req, res) {
 
     if (rows.length > 0) {
         const user = rows[0];
-        const validacion = await helpers.compararContraseña(pass,user.contrasenia);
+        const validacion = await helpers.compararContraseña(pass, user.contrasenia);
         if (validacion) {
-            res.send(JSON.stringify({ retorno: true, mensaje:'Un exito.' }));
+            res.send(JSON.stringify({ retorno: true, mensaje: 'Un exito.' }));
         } else {
-            res.send(JSON.stringify({ retorno: false,mensaje:'Contraseña incorrecta.' }));
+            res.send(JSON.stringify({ retorno: false, mensaje: 'Contraseña incorrecta.' }));
         }
     } else {
-        res.send(JSON.stringify({ retorno: false, mensaje:'No existe este usuario.'}));
+        res.send(JSON.stringify({ retorno: false, mensaje: 'No existe este usuario.' }));
+    }
+};
+
+exports.signup = async function (req, res) {
+
+    var correo = req.param('email');
+    var pass = req.param('password');
+    var fullName = req.param('fullName');
+    var tipo = req.param('tipo');
+
+    const rows = await pool.query('SELECT * FROM usuario WHERE email = ? OR nombreUsuario = ?', [correo, fullName]);
+
+    if (rows.length == 0) {
+        const usuario = {
+            nombreUsuario: fullName,
+            contrasenia: pass,
+            email: correo,
+            estado: true,
+            fotoPerfil_id: null,
+            pais_id: null,
+            ciudad_id: null
+
+        }
+        const user = rows[0];
+        const result = await pool.query('INSERT INTO usuario SET ? ', usuario);
+        if (result) {
+            if (tipo == 0) {
+                const empresa = {
+                    nombre: null,
+                    id: fullName
+                }
+                const resultEmpresa = await pool.query('INSERT INTO empresa SET ? ', empresa);
+                console.log(resultEmpresa);
+                console.log('usuario empresa insertado');
+                res.send(JSON.stringify({ retorno: true, mensaje: 'Un exito.' }));
+            } else {
+                const empleado = {
+                    apellido: null,
+                    celular: null,
+                   // pin: 11,
+                    fechaNacimiento: null,
+                    nombre: null,
+                    id: fullName
+                }
+                const resultEmpleado = await pool.query('INSERT INTO empleado SET ? ', empleado);
+                console.log('usuario empresa insertado');
+                return true;
+            }
+        }
+        res.send(JSON.stringify({ retorno: true, mensaje: 'Un exito.' }));
+    } else {
+        res.send(JSON.stringify({ retorno: false, mensaje: 'Ya existe este usuario.' }));
     }
 };
 
