1609a8b64f0d7db8c7b737c666ad8d998091bcca
Lucas Ciceri <lucasciceri59@gmail.com>
2019-08-06 08:17:34 -0300

    detodo un poco

--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,6 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'tesispu'
+        database: 'tesis'
     }
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,17 +1,55 @@
 const express = require("express");
-var multer = require('multer');
-var upload = multer({dest:'uploads/'});
-
 const { haySession, noHaySession } = require('../lib/session');
 const router = express.Router();
 const usuarioControlador = require("../controladores/usuarioControlador");
+const path = require("path");
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, "../public/img/perfiles"),
+    filename: (req, file, cb) => {
+        const { documento } = req.body;
+        cb(null, documento + path.extname(file.originalname).toLocaleLowerCase());
+    }
+});
 
 router.get('/', usuarioControlador.inicio);
 router.get('/login', noHaySession, usuarioControlador.login);
 router.post('/login', noHaySession, usuarioControlador.iniciar);
 router.get('/logout', haySession, usuarioControlador.salir);
 router.get('/registrarse', noHaySession, usuarioControlador.singin);
-router.post('/signup', upload.single('fotoPerfil'), usuarioControlador.registrarse);
+
+router.post('/signup', multer({
+    storage,
+    limits: { fileSize: 2000000 },
+    fileFilter: (res, file, cb) => {
+        const type = /jpeg|jpg|png/;
+        const mimetype = type.test(file.mimetype);
+        const extname = type.test(path.extname(file.originalname));
+       // const existe = usuarioControlador.checkUser();
+        if (mimetype && extname ) {
+            return cb(null, true);
+        }
+        return cb('Error: Archivo no valido');
+    }
+}).single('fotoPerfil'), usuarioControlador.registrarse);
+
+
+router.post('/update', multer({
+    storage,
+    limits: { fileSize: 2000000 },
+    fileFilter: (res, file, cb) => {
+        const type = /jpeg|jpg|png/;
+        const mimetype = type.test(file.mimetype);
+        const extname = type.test(path.extname(file.originalname));
+        if (mimetype && extname ) {
+            return cb(null, true);
+        }
+        return cb('Error: Archivo no valido');
+    }
+}).single('fotoPerfil'), usuarioControlador.update);
+
+
 
 router.get('/perfil', haySession, usuarioControlador.perfil);
 //recibo los datos
