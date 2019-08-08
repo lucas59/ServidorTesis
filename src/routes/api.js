acbbf67b12f5fbc5a0c218d287e8f1fc495adc46
isma4102 <santoscastelvi2014@gmail.com>
2019-08-08 14:36:09 -0300

    tablet mode

--- src/routes/api.js
+++ src/routes/api.js
@@ -18,12 +18,14 @@ const storage = multer.diskStorage({
 
 router.get('/', apiControlador.inicio);
 router.post('/login', apiControlador.login);
+router.post('/login_tablet', apiControlador.login_tablet);
 router.post('/signup', apiControlador.signup);
 router.post('/signup2', apiControlador.signup2);
 
 router.post('/signup2Empresa', apiControlador.signup2Empresa);
 
 router.post('/Alta_tarea', apiControlador.Alta_tarea);
+router.post('/Alta_asistencia', apiControlador.Alta_asistencia);
 
 router.post('/Tareas/ListaTareas', apiControlador.ListaTareas);
 
