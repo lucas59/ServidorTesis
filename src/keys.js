75b3a239513ba0cbf3358219e66cdb041572f64b
Lucas Ciceri <lucasciceri59@gmail.com>
2019-10-15 20:31:13 -0300

    detalles

--- src/keys.js
+++ src/keys.js
@@ -6,3 +6,4 @@ module.exports = {
         database: 'heroku_fa8cf52710a77df'
     }
 }
+//mysql://be26b926935c48:6e409f14@us-cdbr-iron-east-05.cleardb.net/heroku_fa8cf52710a77df?reconnect=true
--- src/public/js/registrarse.js
+++ src/public/js/registrarse.js
@@ -1,43 +1,76 @@
-let form = document.querySelector('.form-register');
-let progressOptions = document.querySelectorAll('.progressbar__option');
+let form = document.querySelector(".form-register");
+let progressOptions = document.querySelectorAll(".progressbar__option");
 
-form.addEventListener('click', function(e) {
-    let element = e.target;
-    let isButtonNext = element.classList.contains('step__button--next');
-    let isButtonBack = element.classList.contains('step__button--back');
-    if (isButtonNext || isButtonBack) {
-        let currentStep = document.getElementById('step-' + element.dataset.step);
-        let jumpStep = document.getElementById('step-' + element.dataset.to_step);
-        currentStep.addEventListener('animationend', function callback() {
-            currentStep.classList.remove('active');
-            jumpStep.classList.add('active');
-            if (isButtonNext) {
-                currentStep.classList.add('to-left');
-                progressOptions[element.dataset.to_step - 1].classList.add('active');
-            } else {
-                jumpStep.classList.remove('to-left');
-                progressOptions[element.dataset.step - 1].classList.remove('active');
-            }
-            currentStep.removeEventListener('animationend', callback);
-        });
-        currentStep.classList.add('inactive');
-        jumpStep.classList.remove('inactive');
-    }
+form.addEventListener("click", function(e) {
+  let element = e.target;
+  let isButtonNext = element.classList.contains("step__button--next");
+  let isButtonBack = element.classList.contains("step__button--back");
+  if (isButtonNext || isButtonBack) {
+    let currentStep = document.getElementById("step-" + element.dataset.step);
+    let jumpStep = document.getElementById("step-" + element.dataset.to_step);
+    currentStep.addEventListener("animationend", function callback() {
+      currentStep.classList.remove("active");
+      jumpStep.classList.add("active");
+      if (isButtonNext) {
+        currentStep.classList.add("to-left");
+        progressOptions[element.dataset.to_step - 1].classList.add("active");
+      } else {
+        jumpStep.classList.remove("to-left");
+        progressOptions[element.dataset.step - 1].classList.remove("active");
+      }
+      currentStep.removeEventListener("animationend", callback);
+    });
+    currentStep.classList.add("inactive");
+    jumpStep.classList.remove("inactive");
+  }
 });
 
-
 function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-        reader.onload = function(e) {
-            $('#fotoUsuario').attr('src', e.target.result);
-        }
+    reader.onload = function(e) {
+      $("#fotoUsuario").attr("src", e.target.result);
+    };
 
-        reader.readAsDataURL(input.files[0]);
-    }
+    reader.readAsDataURL(input.files[0]);
+  }
 }
 
 $("#inputFoto").change(function() {
-    readURL(this);
+  readURL(this);
 });
+
+$("input[name='tipo']").click(e => {
+  if (e.target.value == "empleado") {
+    $("#inputApellido").show();
+  } else {
+    $("#inputApellido").hide();
+  }
+});
+
+function validarRegistroUsuario() {
+  var nombreuser = document.getElementsByName("username");
+  var email = document.getElementsByName("email");
+  var password = document.getElementsByName("password");
+  var password2 = document.getElementsByName("password2");
+  var nombre = document.getElementsByName("documento");
+  var documento = document.getElementsByName("nombre");
+  var apellido = document.getElementsByName("apellido");
+  var tel = document.getElementsByName("tel");
+
+  if (
+    nombre == "" ||
+    nombreuser == "" ||
+    email == "" ||
+    apellido == "" ||
+    tel == "" ||
+    password == "" ||
+    password2 == "" ||
+    documento == ""
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
