fb0d364125bba51108a921e860eb7fa808c674c3
Lucas Ciceri <lucasciceri59@gmail.com>
2019-12-02 08:44:56 -0300

    Update registrosEmpresas.js

--- src/public/js/registrosEmpresas.js
+++ src/public/js/registrosEmpresas.js
@@ -64,7 +64,7 @@ function mostrarCaptura(btn) {
   var base64 = btn.value;
   console.log(base64);
   $("#modalCaptura").modal("show");
-  var img = "http://localhost:4005/img/asistencias/"+base64+".jpg";
+  var img = "/img/asistencias/"+base64+".jpg";
   $("#imgCaptura").attr("src", img);
 }
 
